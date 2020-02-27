1507e43ecaadff8e5f532809d42f287d7c1b8f79
mittalbhanderi <mitulbhanderi@yahoo.com>
2020-02-27 21:58:32 +0000

    -- adding lazy support

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -6,37 +6,42 @@ import { Route } from 'react-router-dom'
 import App from './App'
 import UserPage from './UserPage'
 import RepoPage from './RepoPage'
-import CounterPage from './CounterPage'
-import AsyncPage from './AsyncPage'
-import ShoppingCart from './ShoppingCart'
-import NodeContainer from './NodeContainer'
-import Autocomplete from './Autocomplete'
+
+const AsyncPage = React.lazy(() => import('./AsyncPage'))
+const CounterPage = React.lazy(() => import('./CounterPage'))
+//import AsyncPage from './AsyncPage'
+const ShoppingCart = React.lazy(() => import('./ShoppingCart'))
+const NodeContainer = React.lazy(() => import('./NodeContainer'))
+const Autocomplete = React.lazy(() => import('./Autocomplete'))
 
 const Root = ({ store }) => (
-  <Provider store={store}>
-    <div>
-      <Route path="/" exact={true} component={App} />
-      <Route path="/counter" exact={true}
-              component={CounterPage} />
-      <Route path="/async" exact={true}
-              component={AsyncPage} />
-      <Route path="/shopping" exact={true}
-              component={ShoppingCart} />
-      <Route path="/tree-view" exact={true}
-              component={NodeContainer} />
-      <Route path="/auto-complete" exact={true}
-              component={Autocomplete} />
-      {/* <Route path="/:login/:name"
+        <Provider store={store}>
+                <div>
+                        <Route path="/" exact={true} component={App} />
+                        <React.Suspense>
+                                <Route path="/counter" exact={true}
+                                        component={CounterPage} />
+                                <Route path="/async" exact={true}
+                                        component={AsyncPage} />
+                                <Route path="/shopping" exact={true}
+                                        component={ShoppingCart} />
+                                <Route path="/tree-view" exact={true}
+                                        component={NodeContainer} />
+                                <Route path="/auto-complete" exact={true}
+                                        component={Autocomplete} />
+                                {/* <Route path="/:login/:name"
              component={RepoPage} />
       <Route path="/:login"
              component={UserPage} /> */}
-      <DevTools />
-    </div>
-  </Provider>
+
+                        </React.Suspense>
+                        <DevTools />
+                </div>
+        </Provider>
 )
 
 Root.propTypes = {
-  store: PropTypes.object.isRequired,
+        store: PropTypes.object.isRequired,
 }
 
 export default Root
